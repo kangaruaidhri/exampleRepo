76ce8fcb9c9857b92e889a3481a97d9ca6bd20b5
Garrett <Garrett@GarrettPC>
2016-08-20 12:48:59 +0100

    This is my first commit!

---
+++ src/com/ruri/test/Ruaidhri.java
@@ -0,0 +1,10 @@
+package com.ruri.test;
+
+public class Ruaidhri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ruaidhri is my name");
+	}
+
+}
